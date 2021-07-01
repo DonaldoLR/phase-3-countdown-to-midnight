e8a344ba305beaa78ff9b5f3e8d93cff0e364d6b
github username <github email address>
2021-07-01 08:46:01 -0400

    Practice looping in ruby and learned the sleep method

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  counter = num
+
+  while counter > 0
+    sleep 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
 end
